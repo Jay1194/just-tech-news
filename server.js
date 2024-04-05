ee58d248c26844196d201f91c246007c20b75424
Jay <j.thomastt1@gmail.com>
2024-04-05 12:33:42 -0700

    partials and helpers

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+const helpers = require("./utils/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 //turn on routes - Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server. js to use.
 app.engine("handlebars", hbs.engine);
